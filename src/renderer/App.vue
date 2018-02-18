ac18259b92426ff50c82626be57f7b8453d2fcd7
Laura-O <laura.ohrndorf@gmail.com>
2018-02-18 01:37:21 +0100

    Add basic navbar

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
+    <navbar></navbar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar';
+
 export default {
     name: 'electron-todo-txt',
+    components: { Navbar },
 };
 </script>
 
---
+++ src/renderer/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+<nav class="navbar">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://bulma.io">
+      <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
+    </a>
+    <a class="navbar-item">
+                        ba
+                    </a>
+    
+  </div>
+</nav>
+  
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+};
+</script>
+
+<style>
+
+</style>
