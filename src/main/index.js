6dd4f903313aebf53bfe96b55cdc27141c9087a9
Laura-O <laura.ohrndorf@gmail.com>
2018-02-20 15:36:30 +0100

    Fix annoying linter error

--- src/main/index.js
+++ src/main/index.js
@@ -1,26 +1,36 @@
-import { app, BrowserWindow } from 'electron' // eslint-disable-line
+import { app, BrowserWindow, Menu, Tray } from 'electron'; // eslint-disable-line
 
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
+
+/* eslint no-underscore-dangle: ["error", { "allow": ["__static"] }] */
 if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\') // eslint-disable-line
+    global.__static = require('path')
+        .join(__dirname, '/static')
+        .replace(/\\/g, '\\\\');
 }
 
 let mainWindow;
-const winURL = process.env.NODE_ENV === 'development'
-    ? 'http://localhost:9080'
-    : `file://${__dirname}/index.html`;
+const winURL =
+    process.env.NODE_ENV === 'development'
+        ? 'http://localhost:9080'
+        : `file://${__dirname}/index.html`;
 
 function createWindow() {
     /**
-   * Initial window options
-   */
+     * Initial window options
+     */
     mainWindow = new BrowserWindow({
-        height: 563,
-        useContentSize: true,
-        width: 1000,
+        // height: 563,
+        // useContentSize: true,
+        // width: 1000,
+        width: 600,
+        height: 600,
+        minWidth: 400,
+        minHeight: 300,
+        resizable: true,
     });
 
     mainWindow.loadURL(winURL);
