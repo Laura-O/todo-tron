2d9868fdec1f8593547f8845d2704e03de5c7d05
Laura-O <laura.ohrndorf@gmail.com>
2018-02-26 14:17:16 +0100

    Remove window attached to menubar

--- src/main/index.js
+++ src/main/index.js
@@ -1,109 +1,98 @@
-import { app, ipcMain, BrowserWindow, Menu, Tray } from 'electron'; // eslint-disable-line
+import { app, ipcMain, BrowserWindow, Menu, Tray } from "electron"; // eslint-disable-line
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
 
 /* eslint no-underscore-dangle: ["error", { "allow": ["__static"] }] */
-if (process.env.NODE_ENV !== 'development') {
-    global.__static = require('path')
-        .join(__dirname, '/static')
-        .replace(/\\/g, '\\\\');
+if (process.env.NODE_ENV !== "development") {
+  global.__static = require("path")
+    .join(__dirname, "/static")
+    .replace(/\\/g, "\\\\");
 }
 
 let mainWindow;
 let tray;
 
 const toggleWindow = (event, bounds) => {
-    if (mainWindow.isVisible()) {
-        mainWindow.hide();
-    } else {
-        // mainWindow.show();
-        const { x, y } = bounds;
-        const { height, width } = mainWindow.getBounds();
-        const yPosition = process.platform === 'darwin' ? y : y - height;
-        const halfWidth = width / 2;
-        mainWindow.setBounds({
-            x: x - halfWidth,
-            y: yPosition,
-            height,
-            width,
-        });
-        mainWindow.show();
-    }
+  if (mainWindow.isVisible()) {
+    mainWindow.hide();
+  } else {
+    mainWindow.show();
+  }
 };
 
 const winURL =
-    process.env.NODE_ENV === 'development'
-        ? 'http://localhost:9080'
-        : `file://${__dirname}/index.html`;
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:9080"
+    : `file://${__dirname}/index.html`;
 
 function createWindow() {
-    /**
-     * Initial window options
-     */
-    mainWindow = new BrowserWindow({
-        height: 600,
-        useContentSize: true,
-        width: 600,
-        autoHideMenuBar: true,
-        frame: false,
-        show: false,
-        title: 'TodoTron 3000',
-        transparent: true,
-        resizable: true,
-        webPreferences: {
-            backgroundThrottling: false,
-        },
-        icon: `${__static}/icons/mac/icon.icns`,
-    });
+  /**
+   * Initial window options
+   */
+  mainWindow = new BrowserWindow({
+    height: 600,
+    useContentSize: true,
+    width: 600,
+    autoHideMenuBar: true,
+    frame: false,
+    show: false,
+    title: "TodoTron 3000",
+    transparent: true,
+    resizable: true,
+    webPreferences: {
+      backgroundThrottling: false
+    },
+    icon: `${__static}/icons/mac/icon.icns`
+  });
 
-    mainWindow.loadURL(winURL);
+  mainWindow.loadURL(winURL);
 
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
 }
 
 function createTray() {
-    tray = new Tray(`${__static}/icons/png/iconTemplate.png`, mainWindow);
+  tray = new Tray(`${__static}/icons/png/iconTemplate.png`, mainWindow);
 
-    tray.on('right-click', () => {
-        mainWindow.hide();
-    });
+  tray.on("right-click", () => {
+    mainWindow.hide();
+  });
 
-    tray.on('click', (event, bounds) => {
-        toggleWindow(event, bounds);
-    });
+  tray.on("click", (event, bounds) => {
+    toggleWindow(event, bounds);
+  });
 }
 
-app.on('ready', () => {
+app.on("ready", () => {
+  createWindow();
+  createTray();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
     createWindow();
-    createTray();
+  }
 });
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+ipcMain.on("update-timer", (event, timeLeft) => {
+  tray.setTitle(timeLeft);
 });
 
-app.on('activate', () => {
-    if (mainWindow === null) {
-        createWindow();
-    }
+ipcMain.on("open-window", () => {
+  mainWindow.show();
 });
 
-ipcMain.on('update-timer', (event, timeLeft) => {
-    tray.setTitle(timeLeft);
-});
-
-ipcMain.on('open-window', () => {
-    mainWindow.show();
-});
-
-ipcMain.on('hide-window', () => {
-    mainWindow.hide();
+ipcMain.on("hide-window", () => {
+  mainWindow.hide();
 });
 
 /**
